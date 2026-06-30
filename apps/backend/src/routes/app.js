e71ddd307c7c9e01733bf8af49e3b82cd1d218f6
joeyvtan <joeytanierga@gmail.com>
2026-06-30 13:38:59 +0800

    feat: add changelog to app version endpoint

--- apps/backend/src/routes/app.js
+++ apps/backend/src/routes/app.js
@@ -9,9 +9,10 @@ const router  = express.Router();
  */
 router.get('/version', (req, res) => {
   res.json({
-    version_code: 1,
-    version_name: '1.0.0',
-    apk_url: null
+    version_code: 2,
+    version_name: '1.1.0',
+    apk_url: https://drive.usercontent.google.com/download?id=1aAR3zZGZ8FUI9iQuq18TRbO5cWM-foRB&export=download&authuser=0
+    changelog: '- Fixed floating timer not draggable when disabled\n- Added draggable home button\n- Fixed USB mode decimal amounts'
   });
 });
 
